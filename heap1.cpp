35428eb94088cde849a1d16aca624c23630079ee
Realadityakumar <adityakumar23214@lpu.in>
2024-11-17 15:18:27 +0530

    learning heap operations such as insertaion, deletiona and heapifying

--- heap1.cpp
+++ heap1.cpp
@@ -28,13 +28,69 @@ class  heap{
         }
         
     }
-    
+    //deletion
+    void deleteFromHeap(){
+        if(size == 0){
+            return;
+        }
+        arr[1] = arr[size];
+        size--;
+        int i = 1;
+        while (i<size){
+            int leftindex = 2*i;
+            int rightindex = 2*i + 1;
+            if(leftindex<size && arr[leftindex] > arr[i]){
+                swap(arr[leftindex],arr[i]);
+                i = leftindex;
+            }
+            else if(rightindex < size && arr[rightindex]>arr[i]){
+                swap(arr[rightindex],arr[i]);
+                i = rightindex;
+            }
+            else{
+                break;
+            }
+        }
+        
+    }
+    //heapify
+    void heapify(int arr[],int n,int i){
+        int largest = i;  // define the largest variable
+        int left = 2*i;
+        int right = 2 * i + 1;
+        // comparing if the left or right is greate who ever is greater just swap with it and recall the function
+        if(left < n && arr[largest]<arr[left]){
+            largest = left;
+        }
+        if(right < n && arr[largest]<arr[right]){
+            largest = right;
+        }
+        if(largest != i){
+            swap(arr[largest],arr[i]);
+            heapify(arr,n,largest);
+        }
+    }
 };
 
 int main(){
-    heap h;
-    h.insert(25);
-    h.insert(45);
-    h.print();
+     heap h;
+    // h.insert(25);
+    // h.insert(45);
+    // h.insert(23);
+    // h.insert(65);
+    // h.insert(34);
+    // h.deleteFromHeap();
+    int arr[100] = {-1,1,5,4,8,6,7,9};
+    int n = 8;
+    for (int i = 1 ; i > n/2; i--){
+        h.heapify(arr,n,i);
+    }
+    
+        for (int i = 1; i <=n; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        
+    
 }
 
